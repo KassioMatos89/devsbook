df987806acd987efef0f9530463c291cb82d17b3
unknown <kassio_29@hotmail.com>
2021-04-06 14:40:40 -0300

    Feature: routes of friends and photos

--- src/views/pages/profile.php
+++ src/views/pages/profile.php
@@ -79,7 +79,7 @@
                             <span>(<?=count($user->following);?>)</span>
                         </div>
                         <div class="box-header-buttons">
-                            <a href="">ver todos</a>
+                            <a href="<?=$base;?>/perfil/<?=$user->id?>/amigos">ver todos</a>
                         </div>
                     </div>
                     <div class="box-body friend-list">
@@ -113,7 +113,7 @@
                             <span>(<?=count($user->photos);?>)</span>
                         </div>
                         <div class="box-header-buttons">
-                            <a href="">ver todos</a>
+                            <a href="<?=$base;?>/perfil/<?=$user->id;?>/fotos">ver todos</a>
                         </div>
                     </div>
                     <div class="box-body row m-20">
