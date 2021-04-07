6f3a3bc43289e2a7856abc9c569ed95493d8616e
unknown <kassio_29@hotmail.com>
2021-04-07 12:11:06 -0300

    Feature: Search route and view

---
+++ src/views/partials/right-side.php
@@ -0,0 +1,17 @@
+<div class="box banners">
+    <div class="box-header">
+        <div class="box-header-text">Patrocinios</div>
+        <div class="box-header-buttons">
+            
+        </div>
+    </div>
+    <div class="box-body">
+        <a href=""><img src="<?=$base;?>/media/advertising/adv1.png" /></a>
+        <a href=""><img src="<?=$base;?>/media/advertising/adv2.jpg" /></a>
+    </div>
+</div>
+<div class="box">
+    <div class="box-body m-10">
+        Criado com ❤️ por B7Web
+    </div>
+</div>
